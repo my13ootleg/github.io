724e49e845d5356c485a308b21831d974bdb577c
Kuki Sashida <my13ootleg@gmail.com>
2022-10-27 15:32:27 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-// スクリーン
 $(function () {
   setTimeout(function () {
     $(".screen").addClass("screen-active", 500);
   }, 500); // 500ミリ秒後に
 });
 
-// ハンバーガー
 $(".toggle-button").on("click", function () {
   $(".toggle-button").toggleClass("toggle-button-active");
 });
@@ -14,60 +12,51 @@ $(".toggle-button").on("click", function () {
   $(".menu").toggleClass("menu-active");
 });
 
-// .contentの領域をクリックして初期化
 $(".content").on("click", function () {
   $(".toggle-button").removeClass("toggle-button-active");
   $(".menu").removeClass("menu-active");
 });
 
-// navの領域をクリックして初期化
 $("nav").on("click", function () {
   $(".toggle-button").removeClass("toggle-button-active");
   $(".menu").removeClass("menu-active");
 });
 
-// ナビゲーションをクリックして初期化
 $("header a").on("click", function () {
   $(".toggle-button").removeClass("toggle-button-active");
   $(".menu").removeClass("menu-active");
 });
 
-// アコーディオン
 $(".works-list").on("click", function () {
-  $(this).next(".works-detail").attr("id", "works-top"); // アコーディオン本体にidを付与する
-  $(this).hide(500, "easeInOutExpo"); // 自分を消す
-  $(".works-list").addClass("works-list-noclick"); // クラスを付与してカードをクリックさせなくする
-  $(".works-detail-close").addClass("works-detail-close-active"); // 閉じるボタンを出す
-  // $(".works-list").hide(1000, "easeInOutExpo"); // 全部消す
-  // $(".works-list").not($(this)).show(1000, "easeInOutExpo"); // 自分以外を表示する
-  $(this).next(".works-detail").slideToggle(1000, "easeInOutExpo"); // スライドして開く
-  $(".works-list").not($(this)).next(".works-detail").slideUp(1000, "easeInOutExpo"); // 他を閉じる
+  $(this).next(".works-detail").attr("id", "works-top");
+  $(this).hide(500, "easeInOutExpo");
+  $(".works-list").addClass("works-list-noclick");
+  $(".works-detail-close").addClass("works-detail-close-active");
+  $(this).next(".works-detail").slideToggle(1000, "easeInOutExpo");
+  $(".works-list").not($(this)).next(".works-detail").slideUp(1000, "easeInOutExpo");
 });
 
 $(".works-detail").on("click", function () {
-  $(".works-detail").removeAttr("id", "works-top"); // アコーディオン本体にidを削除する
-  $(".works-detail").hide(1000, "easeInOutExpo"); // 自分を消す
-  $(".works-list").not($(this)).show(1000, "easeInOutExpo"); // 自分以外を表示する
-  $(".works-list").removeClass("works-list-noclick"); // クラスを削除してカードをクリックできるようにする
-  $(".works-detail-close").removeClass("works-detail-close-active"); // 閉じるボタンを削除する
+  $(".works-detail").removeAttr("id", "works-top");
+  $(".works-detail").hide(1000, "easeInOutExpo");
+  $(".works-list").not($(this)).show(1000, "easeInOutExpo");
+  $(".works-list").removeClass("works-list-noclick");
+  $(".works-detail-close").removeClass("works-detail-close-active");
 });
 
-// アコーディオンの閉じるボタンの動作
 $(".works-detail-close").on("click", function () {
-  $(".works-detail").removeAttr("id", "works-top"); // アコーディオン本体にidを削除する
-  $(".works-detail").hide(1000, "easeInOutExpo"); // 自分を消す
-  $(".works-list").not($(this)).show(1000, "easeInOutExpo"); // 自分以外を表示する
-  $(".works-list").removeClass("works-list-noclick"); // クラスを削除してカードをクリックできるようにする
-  $(".works-detail-close").removeClass("works-detail-close-active"); // 閉じるボタンを削除する
+  $(".works-detail").removeAttr("id", "works-top");
+  $(".works-detail").hide(1000, "easeInOutExpo");
+  $(".works-list").not($(this)).show(1000, "easeInOutExpo");
+  $(".works-list").removeClass("works-list-noclick");
+  $(".works-detail-close").removeClass("works-detail-close-active");
 });
 
-// アコーディオンが開いたらそこまでスクロールする
 $(".works-list").on("click", function () {
   const memberTop = $("#works-top").offset().top - 64;
   $("html").animate({ scrollTop: memberTop }, 1000, "easeInOutExpo");
 });
 
-// scrollreveal
 ScrollReveal().reveal(".content", {
   reset: false,
   delay: 1000,
